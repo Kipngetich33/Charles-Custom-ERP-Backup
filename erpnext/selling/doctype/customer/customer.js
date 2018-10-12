a227d458ecf829bf82fd75795d8d81ad2d67bbfa
Kipngetich33 <khalifngeno@gmail.com>
2018-10-12 14:57:36 +0300

    uncomment all the code in customer before merging remote branch

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -135,10 +135,10 @@ frappe.ui.form.on("Customer", {
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // custom scripts below
-var var_customername
-frappe.ui.form.on("Customer", {
-    onload: function(frm) {
-var_customername=frm.doc.customer_name;
+// var var_customername
+// frappe.ui.form.on("Customer", {
+//     onload: function(frm) {
+// var_customername=frm.doc.customer_name;
 //frappe.call({
 
            // "method": "frappe.client.get",
@@ -149,200 +149,188 @@ var_customername=frm.doc.customer_name;
             //callback: function (r) {
    //frm.set_value("meter_serial_no", r.message.item_code);
 
-frappe.call({
-            "method": "frappe.client.get_value",
-            args: {
-                doctype: "Item",
-               filters: {"item_code":r.message.item_code}   
-            },
-            callback: function (datas) {
-  frm.set_value("meter_serial_no", r.message.item_code);
-  frm.set_value("meter_size", datas.message.item_group);
-  frm.set_value("initial_reading", datas.message.initial_reading);
-//}
-//})
 
 
-}
-});
+/* comment on the funtion below as this functionality should go to
+enquiries*/
+// frappe.call({
+// 	"method": "frappe.client.get_value",
+// 	args: {
+// 		doctype: "Item",
+// 		filters: {"item_code":r.message.item_code}   
+// 	},
+// 	callback: function (datas) {
+// 		frm.set_value("meter_serial_no", r.message.item_code);
+// 		frm.set_value("meter_size", datas.message.item_group);
+// 		frm.set_value("initial_reading", datas.message.initial_reading);
+// 	}
+// });
 
 
-frappe.call({
-            "method": "frappe.client.get_value",
-            args: {
-                doctype: "Item",
- 			fieldname: 'item_group',
-                filters: { item_code: frm.doc.meter_serial_no },
+// frappe.call({
+//             "method": "frappe.client.get_value",
+//             args: {
+//                 doctype: "Item",
+//  			fieldname: 'item_group',
+//                 filters: { item_code: frm.doc.meter_serial_no },
 
-            },
-            callback: function (r) {
+//             },
+//             callback: function (r) {
 
-   frm.set_value("meter_size", r.message.item_group);
-}
-});
-
-
-frappe.call({
-            "method": "frappe.client.get_value",
-            args: {
-                doctype: "Item",
- fieldname: 'initial_reading',
-                filters: { item_code: frm.doc.meter_serial_no },
-            },
-            callback: function (r) {
-   frm.set_value("initial_reading", r.message.initial_reading);
-}
-});
-
-}
-});
+//    frm.set_value("meter_size", r.message.item_group);
+// }
+// });
 
 
-frappe.ui.form.on("Customer", "new_project", function(frm) {
-	/* this code fetches the customer name and the project name and 
-	creates a new project using those details*/
-	frappe.route_options = {"customer": var_customername,"project_name":var_customername +' '+'Connection'}
-	frappe.set_route("Form", "Project","New Project 1")
-});
+// frappe.call({
+//             "method": "frappe.client.get_value",
+//             args: {
+//                 doctype: "Item",
+//  fieldname: 'initial_reading',
+//                 filters: { item_code: frm.doc.meter_serial_no },
+//             },
+//             callback: function (r) {
+//    frm.set_value("initial_reading", r.message.initial_reading);
+// }
+// });
+
+// }
+// });
 
 
-frappe.ui.form.on("Customer", "create_customer_meter_warehouse", function(frm) {
-frappe.route_options = {"customer_name": var_customername}
-frappe.set_route("Form", "Warehouse", "New Warehouse 1")
-}
-);
-
-frappe.ui.form.on("Customer", "make_deposit_invoice", function(frm) {
-frappe.route_options = {"customer": var_customername,"type_of_invoice":"Deposit"}
-frappe.set_route("Form", "Sales Invoice","New Sales Invoice 1")
-
-}
-);
-
-frappe.ui.form.on("Customer", "make_new_connection_fees_invoice", function(frm) {
-frappe.route_options = {"customer": var_customername,"type_of_invoice":"New Connection Fee"}
-frappe.set_route("Form", "Sales Invoice","New Sales Invoice 1")
-}
-);
-
-frappe.ui.form.on("Customer", "new_account_no", function(frm){ 
-cur_frm.clear_table("accounts"); 
-cur_frm.grids[0].grid.add_new_row(null,null,false);
-    var newrow = cur_frm.grids[0].grid.grid_rows[cur_frm.grids[0].grid.grid_rows.length - 1].doc;
-newrow.account=cur_frm.doc.new_account_no;
-cur_frm.refresh_field("accounts") 
-});
-
-frappe.ui.form.on("Customer", {
-    onload: function (frm) {
-cur_frm.clear_table("accounts"); 
-cur_frm.grids[0].grid.add_new_row(null,null,false);
-    var newrow = cur_frm.grids[0].grid.grid_rows[cur_frm.grids[0].grid.grid_rows.length - 1].doc;
-newrow.account=cur_frm.doc.new_account_no;
-cur_frm.refresh_field("accounts")
-    },
-});
+// frappe.ui.form.on("Customer", "new_project", function(frm) {
+// 	/* this code fetches the customer name and the project name and 
+// 	creates a new project using those details*/
+// 	frappe.route_options = {"customer": var_customername,"project_name":var_customername +' '+'Connection'}
+// 	frappe.set_route("Form", "Project","New Project 1")
+// });
 
 
-frappe.ui.form.on('Customer', {
-    refresh: function(frm) {
- frm.add_custom_button(__('Activate'), function(){
-frappe.set_route("Form", "Customer", frm.doc.customer_name);
-frm.set_value("status", "Active")
-cur_frm.save();
- },__("Activate/Inactivate")); 
-   frm.add_custom_button(__('Inactivate'), function(){
-frappe.set_route("Form", "Customer", frm.doc.customer_name);
-frm.set_value("status", "Inactive")
-cur_frm.save();
- }, __("Activate/Inactivate"));
-frm.add_custom_button(__('Disconnect'), function(){
-frappe.set_route('query-report', 'Accounts Receivable', {customer:frm.doc.name});
-			});
- //frappe.set_route("Form", "Customer", frm.doc.customer_name);
-//frm.set_value("status", "Disconnected")
-//cur_frm.save();       
-   // },);
-frm.add_custom_button(__('Reconnect'), function(){
-frappe.set_route("Form", "Customer", frm.doc.customer_name);
-frm.set_value("status", "Reconnected")
-cur_frm.save();       
-    },);
-      frm.add_custom_button(__('Terminate'), function(){
-       frappe.set_route("Form", "Customer", frm.doc.customer_name);
-frm.set_value("status", "Terminated")
-frm.set_df_property("status", "read_only", frm.doc.__islocal ? 0 : 1);
-frm.set_df_property("customer_name", "read_only", frm.doc.__islocal ? 0 : 1);
-cur_frm.save();
-    },); 
- frm.add_custom_button(__('Supercede'), function(){
-frm.copy_doc();
-    },);
-frm.add_custom_button(__('Account Balance'), function(){
-frappe.set_route("Form","Customer","frm.doc.customer_name", frm.doc.outsatanding_balances);
-  },);
-  }
-});
-cur_frm.add_fetch('deposit','standard_rate','deposit_amount')
-cur_frm.add_fetch('meter_serial_no','item_group','meter_size')
+// frappe.ui.form.on("Customer", "create_customer_meter_warehouse", function(frm) {
+// frappe.route_options = {"customer_name": var_customername}
+// frappe.set_route("Form", "Warehouse", "New Warehouse 1")
+// }
+// );
 
-frappe.ui.form.on('Customer', {
-	territory: function(frm) {
-		frm.add_fetch("parent_land_unit", "latitude", "latitude");
-		frm.add_fetch("parent_land_unit", "longitude", "longitude");
-		frm.set_query("parent_land_unit", function() {
-			return {
-				"filters": {
-					"is_group": 1
-				}
-			};
-		});
-	},
+// frappe.ui.form.on("Customer", "make_deposit_invoice", function(frm) {
+// frappe.route_options = {"customer": var_customername,"type_of_invoice":"Deposit"}
+// frappe.set_route("Form", "Sales Invoice","New Sales Invoice 1")
 
-	onload_post_render(frm){
-		if(!frm.doc.location && frm.doc.latitude && frm.doc.longitude)	{
-			frm.fields_dict.location.map.setView([frm.doc.latitude, frm.doc.longitude],13);
+// }
+// );
+
+// frappe.ui.form.on("Customer", "make_new_connection_fees_invoice", function(frm) {
+// frappe.route_options = {"customer": var_customername,"type_of_invoice":"New Connection Fee"}
+// frappe.set_route("Form", "Sales Invoice","New Sales Invoice 1")
+// }
+// );
+
+// frappe.ui.form.on("Customer", "new_account_no", function(frm){ 
+// cur_frm.clear_table("accounts"); 
+// cur_frm.grids[0].grid.add_new_row(null,null,false);
+//     var newrow = cur_frm.grids[0].grid.grid_rows[cur_frm.grids[0].grid.grid_rows.length - 1].doc;
+// newrow.account=cur_frm.doc.new_account_no;
+// cur_frm.refresh_field("accounts") 
+// });
+
+// frappe.ui.form.on("Customer", {
+//     onload: function (frm) {
+// cur_frm.clear_table("accounts"); 
+// cur_frm.grids[0].grid.add_new_row(null,null,false);
+//     var newrow = cur_frm.grids[0].grid.grid_rows[cur_frm.grids[0].grid.grid_rows.length - 1].doc;
+// newrow.account=cur_frm.doc.new_account_no;
+// cur_frm.refresh_field("accounts")
+//     },
+// });
+
+
+// frappe.ui.form.on('Customer', {
+//     refresh: function(frm) {
+//  frm.add_custom_button(__('Activate'), function(){
+// frappe.set_route("Form", "Customer", frm.doc.customer_name);
+// frm.set_value("status", "Active")
+// cur_frm.save();
+//  },__("Activate/Inactivate")); 
+//    frm.add_custom_button(__('Inactivate'), function(){
+// frappe.set_route("Form", "Customer", frm.doc.customer_name);
+// frm.set_value("status", "Inactive")
+// cur_frm.save();
+//  }, __("Activate/Inactivate"));
+// frm.add_custom_button(__('Disconnect'), function(){
+// frappe.set_route('query-report', 'Accounts Receivable', {customer:frm.doc.name});
+// 			});
+//  //frappe.set_route("Form", "Customer", frm.doc.customer_name);
+// //frm.set_value("status", "Disconnected")
+// //cur_frm.save();       
+//    // },);
+// frm.add_custom_button(__('Reconnect'), function(){
+// frappe.set_route("Form", "Customer", frm.doc.customer_name);
+// frm.set_value("status", "Reconnected")
+// cur_frm.save();       
+//     },);
+//       frm.add_custom_button(__('Terminate'), function(){
+//        frappe.set_route("Form", "Customer", frm.doc.customer_name);
+// frm.set_value("status", "Terminated")
+// frm.set_df_property("status", "read_only", frm.doc.__islocal ? 0 : 1);
+// frm.set_df_property("customer_name", "read_only", frm.doc.__islocal ? 0 : 1);
+// cur_frm.save();
+//     },); 
+//  frm.add_custom_button(__('Supercede'), function(){
+// frm.copy_doc();
+//     },);
+// frm.add_custom_button(__('Account Balance'), function(){
+// frappe.set_route("Form","Customer","frm.doc.customer_name", frm.doc.outsatanding_balances);
+//   },);
+//   }
+// });
+// cur_frm.add_fetch('deposit','standard_rate','deposit_amount')
+// cur_frm.add_fetch('meter_serial_no','item_group','meter_size')
+
+// frappe.ui.form.on('Customer', {
+// 	territory: function(frm) {
+// 		frm.add_fetch("parent_land_unit", "latitude", "latitude");
+// 		frm.add_fetch("parent_land_unit", "longitude", "longitude");
+// 		frm.set_query("parent_land_unit", function() {
+// 			return {
+// 				"filters": {
+// 					"is_group": 1
+// 				}
+// 			};
+// 		});
+// 	},
+
+// 	onload_post_render(frm){
+// 		if(!frm.doc.location && frm.doc.latitude && frm.doc.longitude)	{
+// 			frm.fields_dict.location.map.setView([frm.doc.latitude, frm.doc.longitude],13);
+// 		}
+// 		else {
+// 			frm.doc.latitude = frm.fields_dict.location.map.getCenter()['lat'];
+// 			frm.doc.longitude = frm.fields_dict.location.map.getCenter()['lng'];
+// 		}
+// 	},
+// });
+
+
+// test function
+function set_country(){
+	cur_frm.set_query("territory", function() {
+		return {
+			"filters": {
+				"type_of_territory": "Country"
+			}
 		}
-		else {
-			frm.doc.latitude = frm.fields_dict.location.map.getCenter()['lat'];
-			frm.doc.longitude = frm.fields_dict.location.map.getCenter()['lng'];
-		}
-	},
-});
-
+	});
+}
 
 /*Functionality that sets the value of form query 'route' 
 to show only routes*/
 frappe.ui.form.on("Customer", "refresh", function(frm) {
-
-	frappe.call({
-		method: 'frappe.client.get_list',
-		args: {
-			"address_dict": frm.doc.customer_primary_address
-		},
-		callback: function(response) {
-			console.log(response)
-		}
-	});
-	// if(!frm.doc.customer_primary_contact){
-	// 	frm.set_value("mobile_no", "");
-	// 	frm.set_value("email_id", "");
-	// }
-	
-
 	// sets the value of the country/territory query field
-	cur_frm.set_query("territory", function() {
-		return {
-			"filters": {
-				"type": "Country"
-			}
-		}
-	});
 
 	cur_frm.set_query("area", function() {
 		return {
 			"filters": {
-				"type": "Area"
+				"type_of_territory": "Area"
 			}
 		}
 	});
@@ -350,7 +338,7 @@ frappe.ui.form.on("Customer", "refresh", function(frm) {
 	cur_frm.set_query("zone", function() {
 		return {
 			"filters": {
-				"type": "Zone"
+				"type_of_territory": "Zone"
 			}
 		}
 	});
@@ -358,7 +346,7 @@ frappe.ui.form.on("Customer", "refresh", function(frm) {
     cur_frm.set_query("route", function() {
         return {
             "filters": {
-				"type": "Route"
+				"type_of_territory": "Route"
             }
 		};
     });
